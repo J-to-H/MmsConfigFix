bd31621cdeed36b8164b0eefcffc67a4c2fcee0d
J_to_H <becksruly@gmail.com>
2026-05-28 14:59:49 -0400

    Refactor MmsConfigHook to modify URL connection headers

--- app/src/main/java/com/mmsconfigfix/MmsConfigHook.java
+++ app/src/main/java/com/mmsconfigfix/MmsConfigHook.java
@@ -1,8 +1,5 @@
 package com.mmsconfigfix;
 
-import android.os.PersistableBundle;
-import android.telephony.CarrierConfigManager;
-
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -13,72 +10,33 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MmsConfigHook implements IXposedHookLoadPackage {
 
-    private static final int MAX_MESSAGE_SIZE = 1258291;
-    private static final int MAX_IMAGE_WIDTH = 2592;
-    private static final int MAX_IMAGE_HEIGHT = 1944;
-    private static final String PIXEL_UA = "Mozilla/5.0 (Linux; Android 14; Pixel 9 Pro Build/AD1A.240905.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.54 Mobile Safari/537.36";
-    private static final String PIXEL_UA_PROF = "http://uaprof.google.com/pixel9pro.xml";
+    private static final String PIXEL_UA = "Android-Mms/2.0";
+    private static final String PIXEL_UA_PROF = "http://uaprof.google.com/Pixel_9_Pro_uaprof.xml";
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
         if (!lpparam.packageName.equals("com.android.phone")) return;
 
-        // Hook CarrierConfigManager.getConfig()
-        XposedHelpers.findAndHookMethod(
-            CarrierConfigManager.class,
-            "getConfig",
-            new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) {
-                    PersistableBundle config = (PersistableBundle) param.getResult();
-                    if (config == null) config = new PersistableBundle();
-                    config.putInt("maxMessageSize", MAX_MESSAGE_SIZE);
-                    config.putInt("maxImageWidth", MAX_IMAGE_WIDTH);
-                    config.putInt("maxImageHeight", MAX_IMAGE_HEIGHT);
-                    param.setResult(config);
-                }
-            }
-        );
-
-        // Hook CarrierConfigManager.getConfigForSubId()
         try {
             XposedHelpers.findAndHookMethod(
-                CarrierConfigManager.class,
-                "getConfigForSubId",
-                int.class,
+                URL.class,
+                "openConnection",
                 new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) {
-                        PersistableBundle config = (PersistableBundle) param.getResult();
-                        if (config == null) config = new PersistableBundle();
-                        config.putInt("maxMessageSize", MAX_MESSAGE_SIZE);
-                        config.putInt("maxImageWidth", MAX_IMAGE_WIDTH);
-                        config.putInt("maxImageHeight", MAX_IMAGE_HEIGHT);
-                        param.setResult(config);
+                        try {
+                            URLConnection conn = (URLConnection) param.getResult();
+                            String url = ((URL) param.thisObject).toString();
+                            if (url.contains("vtext.com") ||
+                                url.contains("mms") ||
+                                url.contains("63.59")) {
+                                conn.setRequestProperty("x-wap-profile", PIXEL_UA_PROF);
+                                conn.setRequestProperty("User-Agent", PIXEL_UA);
+                            }
+                        } catch (Throwable ignored) {}
                     }
                 }
             );
         } catch (Throwable ignored) {}
-
-        // Hook URL.openConnection() to inject Pixel headers into MMSC requests
-        XposedHelpers.findAndHookMethod(
-            URL.class,
-            "openConnection",
-            new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) {
-                    try {
-                        URLConnection conn = (URLConnection) param.getResult();
-                        String url = ((URL) param.thisObject).toString();
-                        if (url.contains("63.59.138.138") || 
-                            url.contains("vtext.com") || 
-                            url.contains("mms")) {
-                            conn.setRequestProperty("User-Agent", PIXEL_UA);
-                            conn.setRequestProperty("x-wap-profile", PIXEL_UA_PROF);
-                        }
-                    } catch (Throwable ignored) {}
-                }
-            }
-        );
     }
 }
