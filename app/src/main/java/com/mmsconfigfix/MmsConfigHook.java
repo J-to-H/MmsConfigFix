aa17774bb7f5da19c8d9c4dfe74d8af2fb4b5daa
J_to_H <becksruly@gmail.com>
2026-05-28 21:21:23 -0400

    Update MMS user agent profile handling

--- app/src/main/java/com/mmsconfigfix/MmsConfigHook.java
+++ app/src/main/java/com/mmsconfigfix/MmsConfigHook.java
@@ -1,7 +1,6 @@
 package com.mmsconfigfix;
 
-import java.net.URL;
-import java.net.URLConnection;
+import android.content.res.Resources;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -10,8 +9,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MmsConfigHook implements IXposedHookLoadPackage {
 
-    private static final String PIXEL_UA = "Android-Mms/2.0";
-    private static final String PIXEL_UA_PROF = "http://uaprof.google.com/Pixel_9_Pro_uaprof.xml";
+    private static final String MMS_UA_PROF = "http://uaprof.vtext.com/OnePlus/odopcph2583/odopcph2583.xml";
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -19,19 +17,16 @@ public class MmsConfigHook implements IXposedHookLoadPackage {
 
         try {
             XposedHelpers.findAndHookMethod(
-                URL.class,
-                "openConnection",
+                Resources.class,
+                "getString",
+                int.class,
                 new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) {
                         try {
-                            URLConnection conn = (URLConnection) param.getResult();
-                            String url = ((URL) param.thisObject).toString();
-                            if (url.contains("vtext.com") ||
-                                url.contains("mms") ||
-                                url.contains("63.59")) {
-                                conn.setRequestProperty("x-wap-profile", PIXEL_UA_PROF);
-                                conn.setRequestProperty("User-Agent", PIXEL_UA);
+                            String result = (String) param.getResult();
+                            if (result != null && result.contains("sonymobile")) {
+                                param.setResult(MMS_UA_PROF);
                             }
                         } catch (Throwable ignored) {}
                     }
